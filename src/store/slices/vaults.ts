e65eb2ac64da4bdbc139ed28af21cc4c927cb04b
Linkie Link <linkielink.dev@gmail.com>
2023-02-27 21:56:14 +0100

    v1.3.1
    ### Updates
    
    ### New Features
    
    - Feat: Dockerfile enhanced to accept environmental arguments on build
    - Feat: added version number to the footer
    
    ### Fixes and Refactoring
    
    - Fix: APR endpoint for Apollo vaults
    - Fix: mobile farm table data

--- src/store/slices/vaults.ts
+++ src/store/slices/vaults.ts
@@ -158,15 +158,25 @@ export const vaultsSlice = (set: NamedSet<Store>, get: GetState<Store>): VaultsS
       const response = await fetch(networkConfig!.apolloAprUrl)
 
       if (response.ok) {
-        const data: AprResponse[] = await response.json()
+        const data: FlatApr[] | NestedApr[] = await response.json()
 
         const newAprs = data.map((aprData) => {
-          const aprTotal = aprData.apr.reduce((prev, curr) => Number(curr.value) + prev, 0)
-          const feeTotal = aprData.fees.reduce((prev, curr) => Number(curr.value) + prev, 0)
+          try {
+            const apr = aprData as FlatApr
+            const aprTotal = apr.apr.reduce((prev, curr) => Number(curr.value) + prev, 0)
+            const feeTotal = apr.fees.reduce((prev, curr) => Number(curr.value) + prev, 0)
 
-          const finalApr = aprTotal + feeTotal
+            const finalApr = aprTotal + feeTotal
 
-          return { contractAddress: aprData.contract_address, apr: finalApr }
+            return { contractAddress: aprData.contract_address, apr: finalApr }
+          } catch {
+            const apr = aprData as NestedApr
+            const aprTotal = apr.apr.aprs.reduce((prev, curr) => Number(curr.value) + prev, 0)
+            const feeTotal = apr.apr.fees.reduce((prev, curr) => Number(curr.value) + prev, 0)
+
+            const finalApr = aprTotal + feeTotal
+            return { contractAddress: aprData.contract_address, apr: finalApr }
+          }
         })
 
         set({

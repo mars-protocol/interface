7cd44722f6d2ff160d89348cbfaf47a5c460e9dc
Linkie Link <linkielink.dev@gmail.com>
2023-10-10 16:12:12 +0200

    v1.7.0

--- src/configs/osmosis-1.ts
+++ src/configs/osmosis-1.ts
@@ -120,6 +120,7 @@ export const NETWORK_CONFIG: NetworkConfig = {
     creditManager: 'osmo1f2m24wktq0sw3c0lexlg7fv4kngwyttvzws3a3r3al9ld2s2pvds87jqvf',
     accountNft: 'osmo1450hrg6dv2l58c0rvdwx8ec2a0r6dd50hn4frk370tpvqjhy8khqw7sw09',
     pyth: 'osmo13ge29x4e2s63a8ytz2px8gurtyznmue4a69n5275692v3qn3ks8q7cwck7',
+    params: 'osmo1nlmdxt9ctql2jr47qd4fpgzg84cjswxyw6q99u4y4u4q6c2f5ksq7ysent',
   },
   assets: {
     base: ASSETS.osmo,
@@ -147,6 +148,7 @@ export const NETWORK_CONFIG: NetworkConfig = {
   displayCurrency: OTHER_ASSETS.usd,
   appUrl: 'https://app.osmosis.zone',
   isFieldsEnabled: true,
+  hasMultiAssetIncentives: true,
 }
 
 export const VAULT_CONFIGS: Vault[] = [
